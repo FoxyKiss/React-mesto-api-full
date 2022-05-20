2320e92eabc630877031732df8a1ea80df7a8567
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-05-20 13:26:09 +0600

    feat: add logger

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const auth = require('./middlewares/auth');
 const { NotFoundError } = require('./errors/NotFoundError');
 const corsRequest = require('./middlewares/corsRequest');
 const errorsHandler = require('./middlewares/errorsHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // ? Создать Порт и Express сервер
 const { PORT = 3000 } = process.env;
@@ -19,6 +20,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(corsRequest);
+
+app.use(requestLogger);
 
 app.use(helmet);
 
@@ -38,6 +41,7 @@ app.use((req, res, next) => {
   next(new NotFoundError('Адреса по вашему запросу не существует'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsHandler);
 
